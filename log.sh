fbcb129c7117aad4cccec7bdd5e605e01543d674
LeeHanYeong <dev@lhy.kr>
2018-11-09 15:22:20 +0900

    HTTP요청을 받을 시 HTTPS로 redirect되도록 nginx설정 변경

--- log.sh
+++ log.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 LOG_ERROR='/srv/project/.log/error.log'
 LOG_INFO='/srv/project/.log/info.log'
+LOG_NGINX_ERROR='/var/log/nginx/error.log'
+LOG_NGINX_ACCESS='/var/log/nginx/access.log'
 CMD='sudo docker exec $(sudo docker ps -q) tail -f'
-eb ssh --command "$CMD $LOG_ERROR $LOG_INFO"
+eb ssh --command "$CMD $LOG_ERROR $LOG_NGINX_ERROR"
